f4114de331053dda65a8362cfb3921d1d7667969
tokinifubara <teekaymtok@gmail.com>
2019-05-22 12:23:40 +0100

    Changed from local port

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+var PORT = process.env.PORT || 5000;
 var express = require('express');
 var app = express();
-var server = app.listen(8000);
 
+var http = require("http");
+var server = http.Server(app);
 app.use(express.static('public'));
 
-console.log("Server is running on port 8000");
+server.listen(PORT, function() {
+	console.log("Borderritual2.0 is running");
+});
